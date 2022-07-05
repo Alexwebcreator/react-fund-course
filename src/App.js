0b8cd682109dbdf05e83820460d40e525512f7c8
Alexwebcreator <Sanek72Tob@mail.ru>
2022-07-05 15:54:04 +0200

    create components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React, {useState} from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import './styles/app.css';
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState('Текст в инпуте');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'}
+  ]);
 
-    function increment() {
-      setLikes(likes + 1);
-      console.log(likes);
-    }
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'}
+  ]);
 
-    function decrement() {
-      setLikes(likes - 1);
-      console.log(likes);
-    }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input 
-      type="text" 
-      value={value}
-      onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <PostList posts={posts} title="Посты про JS"/>
+      <PostList posts={posts2} title="Посты про Python"/>
     </div>
   );
 }
