ce8948c5cc1ca1bf1f39b71600d069ffa2b64c4f
Alexwebcreator <Sanek72Tob@mail.ru>
2022-07-10 16:49:11 +0200

    create error handler

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import MyInput from './components/UI/input/MyInput';
 import Loader from './components/UI/loader/loader';
 import MyModal from './components/UI/MyModal/MyModal';
 import MySelect from './components/UI/select/MySelect';
+import { useFetching } from './hooks/useFetching';
 import { usePosts } from './hooks/usePosts'; 
 import './styles/app.css';
 
@@ -20,7 +21,10 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -30,13 +34,6 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostsLoading(false);
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
@@ -55,6 +52,9 @@ function App() {
         filter={filter} 
         setFilter={setFilter}
       />
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
       {isPostsLoading
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
